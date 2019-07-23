41638c7616abb3b3c6a2ea30b2a247536932aaf1
Salim Mansour <Salim.Mansour@camh.ca>
2019-07-23 15:49:40 -0400

    Added the rest of the nodes for tractography pipeline

--- dmripreproc/interfaces/mrtrix3.py
+++ dmripreproc/interfaces/mrtrix3.py
@@ -9,7 +9,7 @@ from __future__ import (
 
 import os.path as op
 
-from nipype.interfaces.base import traits, TraitedSpec, File, InputMultiObject, Undefined
+from nipype.interfaces.base import traits, TraitedSpec, File, InputMultiObject, Undefined, CommandLineInputSpec
 from nipype.interfaces.mrtrix3.base import MRTrix3BaseInputSpec, MRTrix3Base
 
 
@@ -752,6 +752,179 @@ class Tractography(MRTrix3Base):
             return trait_spec.argstr % ','.join(value)
 
         return super(Tractography, self)._format_arg(name, trait_spec, value)
+
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        outputs['out_file'] = op.abspath(self.inputs.out_file)
+        return outputs
+
+class TrackSift2InputSpec(MRTrix3BaseInputSpec):
+    in_tracks = File(
+        exists=True,
+        argstr='%s',
+        position=1,
+        mandatory=True,
+        desc='input track file')
+    in_fod = File(
+        exists=True,
+        argstr='%s',
+        position=2,
+        mandatory=True,
+        desc=('input image containing the spherical harmonics of the '
+              'fibre orientation distributions'))
+    out_weights = File(
+        'prob_weights.txt',
+        argstr='%s',
+        position=3,
+        usedefault=True,
+        mandatory=True,
+        desc=('output text file containing the weighting factor for each'
+              'streamline'))
+
+class TrackSift2OutputSpec(TraitedSpec):
+    out_weights = File(argstr='%s', desc='output text file containing the weighting factor for each streamline')
+
+class TrackSift2(MRTrix3Base):
+    _cmd = 'tcksift2'
+    input_spec = TrackSift2InputSpec
+    output_spec = TrackSift2OutputSpec
+
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        outputs['out_weights'] = op.abspath(self.inputs.out_weights)
+        return outputs
+
+class BuildConnectomeInputSpec(CommandLineInputSpec):
+    in_file = File(
+        exists=True,
+        argstr='%s',
+        mandatory=True,
+        position=-3,
+        desc='input tractography')
+    in_parc = File(
+        exists=True, argstr='%s', position=-2, desc='parcellation file')
+    out_file = File(
+        'connectome.csv',
+        argstr='%s',
+        mandatory=True,
+        position=-1,
+        usedefault=True,
+        desc='output file after processing')
+
+    nthreads = traits.Int(
+        argstr='-nthreads %d',
+        desc='number of threads. if zero, the number'
+        ' of available cpus will be used',
+        nohash=True)
+
+    vox_lookup = traits.Bool(
+        argstr='-assignment_voxel_lookup',
+        desc='use a simple voxel lookup value at each streamline endpoint')
+    search_radius = traits.Float(
+        argstr='-assignment_radial_search %f',
+        desc='perform a radial search from each streamline endpoint to locate '
+        'the nearest node. Argument is the maximum radius in mm; if no node is'
+        ' found within this radius, the streamline endpoint is not assigned to'
+        ' any node.')
+    search_reverse = traits.Float(
+        argstr='-assignment_reverse_search %f',
+        desc='traverse from each streamline endpoint inwards along the '
+        'streamline, in search of the last node traversed by the streamline. '
+        'Argument is the maximum traversal length in mm (set to 0 to allow '
+        'search to continue to the streamline midpoint).')
+    search_forward = traits.Float(
+        argstr='-assignment_forward_search %f',
+        desc='project the streamline forwards from the endpoint in search of a'
+        'parcellation node voxel. Argument is the maximum traversal length in '
+        'mm.')
+
+    metric = traits.Enum(
+        'count',
+        'meanlength',
+        'invlength',
+        'invnodevolume',
+        'mean_scalar',
+        'invlength_invnodevolume',
+        argstr='-metric %s',
+        desc='specify the edge'
+        ' weight metric')
+
+    in_scalar = File(
+        exists=True,
+        argstr='-image %s',
+        desc='provide the associated image '
+        'for the mean_scalar metric')
+
+    in_weights = File(
+        exists=True,
+        argstr='-tck_weights_in %s',
+        desc='specify a text scalar '
+        'file containing the streamline weights')
+
+    keep_unassigned = traits.Bool(
+        argstr='-keep_unassigned',
+        desc='By default, the program discards the'
+        ' information regarding those streamlines that are not successfully '
+        'assigned to a node pair. Set this option to keep these values (will '
+        'be the first row/column in the output matrix)')
+
+    zero_diagonal = traits.Bool(
+        argstr='-zero_diagonal',
+        desc='set all diagonal entries in the matrix '
+        'to zero (these represent streamlines that connect to the same node at'
+        ' both ends)')
+
+    scale_length = traits.Bool(
+        argstr='-scale_length',
+        desc='scale each contribution to the connectome edge by the length of the'
+        'streamline')
+
+    scale_invlength = traits.Bool(
+        argstr='-scale_invlength',
+        desc='scale each contribution to the connectome edge by the inverse of the'
+        'streamline length')
+
+    scale_invnodevol = traits.Bool(
+        argstr='-scale_invnodevol',
+        desc='scale each contribution to the connectome edge by the inverse of the two'
+        'node volumes')
+
+    symmetric = traits.Bool(
+        argstr='-symmetric',
+        desc='Make matrices symmetric on output')
+
+    stat_edge = traits.Enum(
+        'sum',
+        'mean',
+        'min',
+        'max',
+        argstr='-stat_edge %s',
+        desc='statistic for combining the values from all streamlines in an edge into a'
+        'single scale value for that edge (options are: sum,mean,min,max;'
+        'default=sum)')
+
+
+class BuildConnectomeOutputSpec(TraitedSpec):
+    out_file = File(exists=True, desc='the output response file')
+
+class BuildConnectome(MRTrix3Base):
+    """
+    Generate a connectome matrix from a streamlines file and a node
+    parcellation image
+    Example
+    -------
+    >>> import nipype.interfaces.mrtrix3 as mrt
+    >>> mat = mrt.BuildConnectome()
+    >>> mat.inputs.in_file = 'tracks.tck'
+    >>> mat.inputs.in_parc = 'aparc+aseg.nii'
+    >>> mat.cmdline                               # doctest: +ELLIPSIS
+    'tck2connectome tracks.tck aparc+aseg.nii connectome.csv'
+    >>> mat.run()                                 # doctest: +SKIP
+    """
+
+    _cmd = 'tck2connectome'
+    input_spec = BuildConnectomeInputSpec
+    output_spec = BuildConnectomeOutputSpec
 
     def _list_outputs(self):
         outputs = self.output_spec().get()
--- dmripreproc/workflows/tractography/base.py
+++ dmripreproc/workflows/tractography/base.py
@@ -13,31 +13,44 @@ from bids import BIDSLayout
 
 from ...interfaces import mrtrix3
 from ...interfaces import fsl as dmri_fsl
+from .outputs import init_tract_output_wf
 
 from nipype.interfaces import fsl, utility as niu
 
 def init_tract_wf():
+    output_dir = '/scratch/smansour/tract_testing/'
 
     tract_wf = pe.Workflow(name="tract_wf")
-    tract_wf.base_dir = '/scratch/smansour/tract_testing/scratch'
+    tract_wf.base_dir = os.path.join(output_dir, 'scratch')
 
     inputnode = pe.Node(
         niu.IdentityInterface(
             fields=[
                 "subject_id",
+                "session_id",
+                "output_dir",
                 "t1_file",
                 "eddy_file",
                 "dwi_mask",
                 "bval",
                 "bvec",
-                "bvecbval",
+                "template",
+                "atlas",
             ]
         ),
         name="inputnode",
     )
 
     outputnode = pe.Node(
-        niu.IdentityInterface(fields=["out_file"]),
+        niu.IdentityInterface(
+            fields=[
+                "tck_file",
+                "prob_weights",
+                "shen_diff_space",
+                "inv_len_conmat",
+                "len_conmat",
+            ]
+        ),
         name="outputnode",
     )
 
@@ -60,22 +73,43 @@ def init_tract_wf():
     estimateFOD = pe.Node(mrtrix3.EstimateFOD(algorithm='csd', wm_odf='FOD.mif'), name="estimateFOD")
     # perform probabilistic tractography
     #tckgen FOD.mif prob.tck -act 5TT.mif -seed_gmwmi gmwmi.mif -select 5000000 ## seeding from a binarised gmwmi
-    tckgen = pe.Node(mrtrix3.Tractography(select=5000000), name="tckgen")
+    tckgen = pe.Node(mrtrix3.Tractography(select=5000), name="tckgen")
     #mrview data.nii.gz -tractography.load prob.tck
 
+    def gen_tuple(item1, item2):
+        return (item1, item2)
+
+    gen_tuple = pe.Node(
+        niu.Function(
+            input_names=["item1", "item2"],
+            output_names=["out_tuple"],
+            function=gen_tuple,
+        ),
+        name="gen_tuple",
+    )
     #use sift to filter tracks based on spherical harmonics
     #tcksift2 prob.tck FOD.mif prob_weights.txt
+    tcksift = pe.Node(mrtrix3.TrackSift2(out_weights="prob_weights.txt"), name="tcksift")
 
     ## atlas reg
     #flirt -in T1w_brain.nii.gz -ref MNI152_T1_1mm_brain.nii.gz -omat xformT1_2_MNI.mat
+    pre_atlas_flirt = pe.Node(fsl.FLIRT(), name="pre_atlas_flirt")
     #convert_xfm -omat xformMNI_2_T1.mat -inverse xformT12MNI.mat
+    xfm_inv = pe.Node(fsl.ConvertXFM(invert_xfm=True), name="xfm_inv")
     #convert_xfm -omat xformMNI_2_diff.mat -concat xformT1_2_diff.mat xformMNI_2_T1.mat
+    xfm_concat = pe.Node(fsl.ConvertXFM(concat_xfm=True), name="xfm_concat")
 
     #flirt -in shen268.nii.gz -ref T1_diff.nii.gz -applyxfm -init xformMNI_2_diff.mat -interp nearestneighbour -out shen_diff_space.nii.gz
+    atlas_flirt = pe.Node(fsl.FLIRT(apply_xfm=True, interp='nearestneighbour'), name="atlas_flirt")
 
     ## generate connectivity matrices
     #tck2connectome prob.tck shen_diff_space.nii.gz conmat_shen.csv -scale_invlength -zero_diagonal -symmetric -tck_weights_in prob_weights.txt -assignment_radial_search 2 -scale_invnodevol
+    conmatgen = pe.Node(mrtrix3.BuildConnectome(out_file="conmat_shen.csv", scale_invlength=True, symmetric=True, zero_diagonal=True, search_radius=2, scale_invnodevol=True), name="conmatgen")
     #tck2connectome prob.tck shen_diff_space.nii.gz conmat_length_shen.csv -zero_diagonal -symmetric -scale_length -stat_edge mean -assignment_radial_search 2
+    conmatgen2 = pe.Node(mrtrix3.BuildConnectome(out_file="conmat_length_shen.csv", scale_length=True, symmetric=True, zero_diagonal=True, search_radius=2, stat_edge='mean'), name="conmatgen2")
+
+    # Initialize output wf
+    datasink = init_tract_output_wf()
 
     tract_wf.connect(
         [
@@ -91,24 +125,85 @@ def init_tract_wf():
             (gen5tt, gen5ttMask, [("out_file", "in_file")]),
             (
                 inputnode,
+                gen_tuple,
+                [
+                    ("bvec", "item1"),
+                    ("bval", "item2")
+                ]
+            ),
+            (gen_tuple, responseSD, [("out_tuple", "grad_fsl")]),
+            (
+                inputnode,
                 responseSD,
                 [
                     ("eddy_file", "in_file"),
-                    ("bvecbval", "grad_fsl")
                 ]
             ),
+            (gen_tuple, estimateFOD, [("out_tuple", "grad_fsl")]),
             (
                 inputnode,
                 estimateFOD,
                 [
                     ("eddy_file", "in_file"),
-                    ("bvecbval", "grad_fsl")
                 ]
             ),
             (responseSD, estimateFOD, [("wm_file", "wm_txt")]),
+            # tckgen
             (estimateFOD, tckgen, [("wm_odf", "in_file")]),
             (gen5tt, tckgen, [("out_file", "act_file")]),
-            (gen5ttMask, tckgen, [("out_file", "seed_gmwmi")])
+            (gen5ttMask, tckgen, [("out_file", "seed_gmwmi")]),
+            # tcksift
+            (estimateFOD, tcksift, [("wm_odf", "in_fod")]),
+            (tckgen, tcksift, [("out_file", "in_tracks")]),
+            # atlas flirt
+            (
+                inputnode,
+                pre_atlas_flirt,
+                [
+                    ("t1_file", "in_file"),
+                    ("template", "reference")
+                ]
+            ),
+            (pre_atlas_flirt, xfm_inv, [("out_matrix_file", "in_file")]),
+            (flirt, xfm_concat, [("out_matrix_file", "in_file2")]),
+            (xfm_inv, xfm_concat, [("out_file", "in_file")]),
+            # Shen atlas register
+            (inputnode, atlas_flirt, [("atlas", "in_file")]),
+            (flirt, atlas_flirt, [("out_file", "reference")]),
+            (xfm_concat, atlas_flirt, [("out_file", "in_matrix_file")]),
+            # generate connectivity matrices
+            (tckgen, conmatgen, [("out_file", "in_file")]),
+            (atlas_flirt, conmatgen, [("out_file", "in_parc")]),
+            (tcksift, conmatgen, [("out_weights", "in_weights")]),
+            (tckgen, conmatgen2, [("out_file", "in_file")]),
+            (atlas_flirt, conmatgen2, [("out_file", "in_parc")]),
+            # outputnode
+            (tckgen, outputnode, [("out_file", "tck_file")]),
+            (tcksift, outputnode, [("out_weights", "prob_weights")]),
+            (atlas_flirt, outputnode, [("out_file", "shen_diff_space")]),
+            (conmatgen, outputnode, [("out_file", "inv_len_conmat")]),
+            (conmatgen2, outputnode, [("out_file", "len_conmat")]),
+            # datasink
+            (
+                inputnode,
+                datasink,
+                [
+                    ("subject_id", "inputnode.subject_id"),
+                    ("session_id", "inputnode.session_id"),
+                    ("output_dir", "inputnode.output_folder")
+                ]
+            ),
+            (
+                outputnode,
+                datasink,
+                [
+                    ("tck_file", "inputnode.tck_file"),
+                    ("prob_weights", "inputnode.prob_weights"),
+                    ("shen_diff_space", "inputnode.shen_diff_space"),
+                    ("inv_len_conmat", "inputnode.inv_len_conmat"),
+                    ("len_conmat", "inputnode.len_conmat")
+                ]
+            ),
         ]
     )
     return tract_wf
